b8b7a3ad8cc00e00a50641e9fe9a379798336bff
Rizwan Haider <rizwan.haider@ferrum.network>
2022-02-04 17:33:35 +0500

    Gw-52: add search by query string

--- app/controllers/api/v1/networks.js
+++ app/controllers/api/v1/networks.js
@@ -2,9 +2,9 @@ const { db, asyncMiddleware, commonFunctions, stringHelper } = global
 
 module.exports = function (router) {
     
-    router.get('/by/ferrum/identifier/:identifier', async (req, res) => {
+    router.get('/by/ferrum/identifier', async (req, res) => {
 
-        var filter =  { ferrumNetworkIdentifier: req.params.identifier }   
+        var filter =  { ferrumNetworkIdentifier: req.query.ferrumNetworkIdentifier }   
         let network = await db.Networks.findOne(filter)         
         return network ? res.http200({ network }) : res.http404('Network not found') 
     
