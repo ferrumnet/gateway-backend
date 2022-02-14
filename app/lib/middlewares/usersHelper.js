186e3c3ddd6d4cb3f29bb9aa9177b9f595a343fe
Zikriya Mushtaq <zikriamushtaq12@gmail.com>
2022-02-14 18:38:21 +0500

    basiq structure created for logs according to the new flow

--- app/lib/middlewares/usersHelper.js
+++ app/lib/middlewares/usersHelper.js
@@ -1,9 +1,12 @@
-const { db, asyncMiddleware, commonFunctions, stringHelper, timeoutHelper, timeoutCallBack, utils } = global
+const { db, asyncMiddleware, commonFunctions, stringHelper, timeoutHelper, timeoutCallBack, utils, logsHelper } = global
 var mongoose , {isValidObjectId} = require('mongoose');
 
 module.exports = {
 
   async signOut(req, res) {
+    if(req.body && req.body.log){
+      await logsHelper.createLog(req, res, true)
+    }
     return res.http200({
       message: stringHelper.strSuccess
     });
