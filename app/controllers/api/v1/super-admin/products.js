67ce04412cab75dee64356b6445c7d8206f5c1ea
Rizwan Haider <rizwan.haider@ferrum.network>
2022-02-25 17:10:57 +0500

    accommodating change request

--- app/controllers/api/v1/super-admin/products.js
+++ app/controllers/api/v1/super-admin/products.js
@@ -3,6 +3,7 @@ const productHelper = require('../../../../lib/middlewares/helpers/productHelper
 
 const { isValidObjectId } = require("mongoose");
 module.exports = function (router) {
+
   router.get("/list", async (req, res) => {
     const filter = utils.pick(req.query, ["name"]);
     const options = utils.pick(req.query, ["sortBy", "limit", "page"]);
@@ -11,71 +12,69 @@ module.exports = function (router) {
   });
 
   router.post("/create", async (req, res) => {
-    if (req.body.name) {
-      const product = await productHelper.create(req.body, req.user.id);
+    const payload = {name:req.body.name, createdBy:req.user.id};
+    if (payload.name) {      
+      const product = await db.Product.create(payload);
       return res.http200({ product });
-    } else {
-      return res.http400("Name is required.");
     }
+      return res.http400("Name is required.");
+    
   });
 
   router.put("/update/:id", async (req, res) => {
-    if (req.body.name && isValidObjectId(req.params.id)) {
-      const productCount = await productHelper.countById(req.params.id);
+    const filter = { _id:req.params.id };
+    const payload = {name: req.body.name, createdBy:req.user._id }   
+   
+    if (payload.name && isValidObjectId(filter._id)) {
+      const productCount = await productHelper.countById(filter._id);
       if (productCount > 0) {
-        const product = await productHelper.updateById(
-          req.params.id,
-          req.body.name,
-          req.user._id
-        );
+        const product =  await db.Product.findOneAndUpdate(filter, payload, { new: true })
         return res.http200({ product });
-      } else {
-        return res.http404(
-          await commonFunctions.getValueFromStringsPhrase(
-            stringHelper.strErrorProductNotFound
-          ),
+      } 
+      return res.http404(
+          await commonFunctions.getValueFromStringsPhrase(stringHelper.strErrorProductNotFound),
           stringHelper.strErrorProductNotFound
-        );
-      }
-    } else {
+        );      
+    } 
       return res.http400("Name and valid ID is required.");
-    }
+    
   });
 
   router.put("/active/inactive/:id", async (req, res) => {
-    if (isValidObjectId(req.params.id) && typeof req.body.active == "boolean") {
-      const productCount = await productHelper.countById(req.params.id);
+    const filter = { _id: req.params.id };
+    const payload = {isActive: req.body.active};
+
+    if (isValidObjectId(filter._id) && typeof payload.isActive == "boolean") {
+      const productCount = await db.Product.countDocuments(filter);
+      
       if (productCount > 0) {
-        const product = await productHelper.updateActivationById(req);
+        const product = await db.Product.findOneAndUpdate(filter, payload, { new: true });               
         return res.http200({ product });
-      } else {
+      } 
         return res.http404(
-          await commonFunctions.getValueFromStringsPhrase(
-            stringHelper.strErrorProductNotFound
-          ),
+           await commonFunctions.getValueFromStringsPhrase(stringHelper.strErrorProductNotFound ),
           stringHelper.strErrorProductNotFound
         );
-      }
-    } else {
-      return res.http400("Valid id and active is required.");
+      
     }
+      return res.http400("Valid id and active is required.");
+    
   });
 
   router.get("/:id", async (req, res) => {
-    if (isValidObjectId(req.params.id)) {
-      const product = await productHelper.getById(req.params.id);
+    const filter = { _id:req.params.id };
+    if (isValidObjectId(filter._id)) {
+      const product = await  db.Product.findOne(filter);
       if (product) {
         return res.http200({ product });
-      } else {
+      } 
         return res.http404(
-          await commonFunctions.getValueFromStringsPhrase(
-            stringHelper.strErrorProductNotFound
-          ),
+          await commonFunctions.getValueFromStringsPhrase(stringHelper.strErrorProductNotFound ),
           stringHelper.strErrorProductNotFound
         );
-      }
-    } else {
+      
+    } 
       return res.http400("Valid id is required.");
-    }
+    
   });
 };
--- app/lib/middlewares/helpers/productHelper.js
+++ app/lib/middlewares/helpers/productHelper.js
@@ -1,17 +1,5 @@
 const { db } = global;
 
-const create = async (reqBody, createBy) => {
-  let payload = {};
-  payload.name = reqBody.name;
-  payload.createdBy = createBy;
-  return await db.Product.create(payload);
-};
-
-const updateById = async (_id, name, createdBy) => {
-  const filter = { _id };
-  let payload = { name, createdBy };
-  return await db.Product.findOneAndUpdate(filter, payload, { new: true });
-};
 
 const updateActivationById = async (req) => {
   const filter = { _id: req.params.id };
@@ -29,6 +17,7 @@ const countById = async (_id) => {
   const filter = { _id };
   return await db.Product.countDocuments(filter);
 };
+
 
 const queryProducts = async (filter, options) => {
   return await db.Product.paginate(filter, options);
