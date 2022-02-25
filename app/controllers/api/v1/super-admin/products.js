0f5cbf0b5860a907dccfef69661cc358ac4fe8c0
Rizwan Haider <rizwan.haider@ferrum.network>
2022-02-25 21:43:37 +0500

    listing updated

--- app/controllers/api/v1/super-admin/products.js
+++ app/controllers/api/v1/super-admin/products.js
@@ -4,19 +4,33 @@ const { asyncMiddleware, commonFunctions, utils } = global;
 const { isValidObjectId } = require("mongoose");
 module.exports = function (router) {
 
-  router.get("/list", asyncMiddleware(async (req, res) => {
-    const filter = req.query.name ? {'nameInLower': { $regex: '.*' + (req.query.name).toLowerCase() + '.*' }} : {} 
-    const products = await db.Product.find(filter)
+  router.get("/list", asyncMiddleware(async (req, res) => {   
+    var filter = {}  
+    let products = []
+
+    if(req.query.isActive){
+      filter.isActive = req.query.isActive
+    } 
+    if (req.query.search) {
+      let reg = new RegExp(unescape(req.query.search), 'i');
+      filter.name = reg
+    }         
+    if(req.query.isPagination != null && req.query.isPagination == 'false'){
+      products = await db.Product.find(filter)
+      .sort({ createdAt: -1 })
+    }else {
+      products = await db.Product.find(filter)
       .sort({ createdAt: -1 })
       .skip(req.query.offset ? parseInt(req.query.offset) : 0)
-      .limit(req.query.limit ? parseInt(req.query.limit) : 10);
-    return res.http200({ products });
+      .limit(req.query.limit ? parseInt(req.query.limit) : 10)
+    }
+    return res.http200({products})
+ 
   }));
 
   router.post("/create", asyncMiddleware(async (req, res) => {
     const payload = {name:req.body.name, createdByUser:req.user.id};
     if (payload.name) { 
-
       const product = await db.Product.create(payload);
       return res.http200({ product });
     }
