c35be321fe9f5dca0754797e6b7c7fda4f425c4d
Zikriya Mushtaq <zikriamushtaq12@gmail.com>
2022-03-31 16:46:47 +0500

    bug fixed at getCompetitionParticipants

--- app/lib/crons/fetchCompetitionTransactionsJob.js
+++ app/lib/crons/fetchCompetitionTransactionsJob.js
@@ -12,15 +12,15 @@ module.exports =  async function () {
      //let startBlock = await bscScanHelper.queryBlockNumber(getTimeStamp());
      await cTSnapshotHelper.createSnapshotMeta('0xa719b8ab7ea7af0ddb4358719a34631bb79d15dc', '16309980');
      //end temporary conditions
- 
+
      let isLock = false
      cron.schedule("*/2 * * * *", async () => {
        if(!isLock){
-         isLock = true;  
+         isLock = true;
          await transactionSnapshotJob();
-         isLock = false 
+         isLock = false
        }
-     });  
+     });
   }catch(error){
     console.log(error)
   }
@@ -28,36 +28,36 @@ module.exports =  async function () {
 };
 
 const transactionSnapshotJob = async () => {
-  try {       
-      //for future when we will have multi cabns 
+  try {
+      //for future when we will have multi cabns
       // approch 1
       //1: get active cabns from active competitions
       //2:  store in snapshotMeta
-      // alternative 
-      //1: on time of activate competion register with cTSnapshotHelper.createSnapshotMeta  
+      // alternative
+      //1: on time of activate competion register with cTSnapshotHelper.createSnapshotMeta
       console.log('cron')
-      snapshotMetas = await cTSnapshotHelper.getActiveSnapshotMetas();    
-      console.log('snapshotMetas.lenth' , snapshotMetas.length)  
+      snapshotMetas = await cTSnapshotHelper.getActiveSnapshotMetas();
+      console.log('snapshotMetas.lenth' , snapshotMetas.length)
 
-      if(snapshotMetas.length > 0){      
+      if(snapshotMetas.length > 0){
          let endBlock = await calculateEndBlockNumber();
          if(typeof endBlock === 'number'){
           for(let i= 0; i<snapshotMetas.length; i++){
-                    
+
             let transations = await bscScanHelper.queryByCABN(snapshotMetas[i].tokenContractAddress, snapshotMetas[i].currentBlockNumber, endBlock.toString());
             //let transations = await bscScanHelper.queryByCABN(snapshotMetas[i].tokenContractAddress, "16310378", "16315222");
             console.log('transations.length',transations.length)
-            if(transations.length > 0){                                
-              await cTSnapshotHelper.insertTransactionsSnapshot(transations);                        
-              await competitionGrowthTrackerJob(snapshotMetas[i].tokenContractAddress, transations, endBlock)                      
-            }           
+            if(transations.length > 0){
+              await cTSnapshotHelper.insertTransactionsSnapshot(transations);
+              await competitionGrowthTrackerJob(snapshotMetas[i].tokenContractAddress, transations, endBlock)
+            }
             await cTSnapshotHelper.updateMetaByContractAddress(snapshotMetas[i].tokenContractAddress, snapshotMetas[i].currentBlockNumber, endBlock);
-            console.log('job compeleted')                    
+            console.log('job compeleted')
           }
          }
-        
-      } 
-       
+
+      }
+
   } catch (e) {
     console.log(e);
   }
@@ -69,14 +69,14 @@ const competitionGrowthTrackerJob = async(tokenContractAddress, transactions, en
    let competions = await competitionHelper.getActiveCompetitionForGrowth(tokenContractAddress);
    console.log('competions.lenght=> ',competions.length)
    for(let i=0; i<competions.length;i++){
-    let participants = await CGTrackerHelper.getCompetitionParticipants(tokenContractAddress, competions[i]._id)    
+    let participants = await CGTrackerHelper.getCompetitionParticipants(tokenContractAddress, competions[i]._id, competions[i])
     console.log('participants.lenght=> ',participants.length)
     if(participants.length>0){
-      let participantsGrowth = await calcaluteGrowthVolume( "tradingVolumeFlow", transactions, participants, competions[i].dexLiquidityPoolCurrencyAddressByNetwork, competions[i]._id ); 
+      let participantsGrowth = await calcaluteGrowthVolume( "tradingVolumeFlow", transactions, participants, competions[i].dexLiquidityPoolCurrencyAddressByNetwork, competions[i]._id );
       console.log('participantsGrowth.lenght=> ',participantsGrowth.length)
-      await CGTrackerHelper.storeCompetitionGrowth(tokenContractAddress, competions[i]._id, participantsGrowth) 
-      await competitionHelper.updateCompetitionCurrentBlock(competions[i]._id, endBlock)         
-    }      
+      await CGTrackerHelper.storeCompetitionGrowth(tokenContractAddress, competions[i]._id, participantsGrowth)
+      await competitionHelper.updateCompetitionCurrentBlock(competions[i]._id, endBlock)
+    }
   }
 }
 };
@@ -85,9 +85,9 @@ const getTimeStamp = () => {
   return Date.now().toString().substring(0, 10);
 };
 
-const calculateEndBlockNumber = async()=>{  
+const calculateEndBlockNumber = async()=>{
   let endBlock = await bscScanHelper.queryBlockNumber(getTimeStamp());
   endBlock = parseInt(endBlock);
   --endBlock
-  return endBlock 
+  return endBlock
 }
