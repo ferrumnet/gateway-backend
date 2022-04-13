a62b2ed1105e841fa2ad89e1833309f5abb664f2
Rizwan Haider <rizwan.haider@ferrum.network>
2022-04-13 20:14:33 +0500

    api to mark trasactions block as verified

--- app/controllers/api/v1/super-admin/competitionTransactionsSnapshot.js
+++ app/controllers/api/v1/super-admin/competitionTransactionsSnapshot.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 
 module.exports = function (router) {
 
-  router.get('/bloackRange/verified/:verified', asyncMiddleware(async (req, res) => {
+  router.get('/blockNumberRange/verified/:verified', asyncMiddleware(async (req, res) => {
     let filter = {}
     if(req.params.verified === 'true'){
         filter = {'$match':{ 'verifiedBlockNumber': true}}
@@ -14,16 +14,41 @@ module.exports = function (router) {
             }
           }
     }
-
-   let range = await db.CompetitionTransactionsSnapshots.aggregate([ filter, {
+   let transactionsByContractAddress = await db.CompetitionTransactionsSnapshots.aggregate([ filter, {
           '$group': {
-            '_id': '$verifiedBlockNumber', 
+            '_id': '$contractAddress', 
             'toBlock': {'$max': '$blockNumber'}, 
             'fromBlock': {'$min': '$blockNumber'}, 
             'transactions': {'$sum': 1}
           }
         }
       ])
-    return res.http200({range})
+      let totalTransactionsCount = 0
+      transactionsByContractAddress.forEach(record=>{
+        totalTransactionsCount += record.transactions
+      })
+    
+    return res.http200({totalTransactionsCount, transactionsByContractAddress})
   }));
+
+  router.post('/blockNumbers/verify', asyncMiddleware(async (req, res) => { 
+    const transactionsCount = parseInt(req.body.transactionsCount)
+    const fromBlockNumber = req.body.fromBlockNumber
+    const toBlockNumber = req.body.toBlockNumber
+
+   if(transactionsCount && fromBlockNumber && toBlockNumber){
+    let filter = {$or:[{'verifiedBlockNumber': null}, {'verifiedBlockNumber': false}], blockNumber:{$gte: fromBlockNumber}, blockNumber:{$lte: toBlockNumber}}
+    const count = await db.CompetitionTransactionsSnapshots.countDocuments( filter)
+    if(transactionsCount == count){        
+       await db.CompetitionTransactionsSnapshots.updateMany(
+            { blockNumber: { $gte: fromBlockNumber}, blockNumber:{$lte:toBlockNumber } },
+            { $set: { "verifiedBlockNumber" : true } }
+         );
+         return res.http200('blockNumberRange verified successfully')
+    }
+    return res.http400('Wrong TransactionsCount')
+   }
+   return res.http400('TransactionsCount, fromBlockNumber and toBlockNumber required')
+  }));
+
 }
