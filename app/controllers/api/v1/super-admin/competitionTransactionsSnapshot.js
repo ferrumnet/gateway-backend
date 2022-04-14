6355f0957b03e05e15addb29272438e977291dfa
Rizwan Haider <rizwan.haider@ferrum.network>
2022-04-14 18:25:08 +0500

    Recapture transactions from bscScan for competition transactions snapshot

--- app/controllers/api/v1/super-admin/competitionTransactionsSnapshot.js
+++ app/controllers/api/v1/super-admin/competitionTransactionsSnapshot.js
@@ -1,4 +1,4 @@
-const { db, asyncMiddleware, commonFunctions, stringHelper } = global
+const { db, asyncMiddleware, commonFunctions, stringHelper, cTSnapshotHelper, bscScanHelper } = global
 var mongoose = require('mongoose');
 
 module.exports = function (router) {
@@ -35,20 +35,51 @@ module.exports = function (router) {
     const transactionsCount = parseInt(req.body.transactionsCount)
     const fromBlockNumber = req.body.fromBlockNumber
     const toBlockNumber = req.body.toBlockNumber
-
-   if(transactionsCount && fromBlockNumber && toBlockNumber){
-    let filter = {$or:[{'verifiedBlockNumber': null}, {'verifiedBlockNumber': false}], blockNumber:{$gte: fromBlockNumber}, blockNumber:{$lte: toBlockNumber}}
-    const count = await db.CompetitionTransactionsSnapshots.countDocuments( filter)
-    if(transactionsCount == count){        
-       await db.CompetitionTransactionsSnapshots.updateMany(
+   
+    if(transactionsCount && fromBlockNumber && toBlockNumber){
+      let filter = {$or:[{'verifiedBlockNumber': null}, {'verifiedBlockNumber': false}], blockNumber:{$gte: fromBlockNumber}, blockNumber:{$lte: toBlockNumber}}
+      const count = await db.CompetitionTransactionsSnapshots.countDocuments( filter)    
+      if(transactionsCount == count){              
+        await db.CompetitionTransactionsSnapshots.updateMany(
             { blockNumber: { $gte: fromBlockNumber}, blockNumber:{$lte:toBlockNumber } },
             { $set: { "verifiedBlockNumber" : true } }
          );
          return res.http200('blockNumberRange verified successfully')
-    }
-    return res.http400('Wrong TransactionsCount')
-   }
-   return res.http400('TransactionsCount, fromBlockNumber and toBlockNumber required')
+        }
+        return res.http400('Wrong TransactionsCount')  
+      }  
+      return res.http400('TransactionsCount, fromBlockNumber and toBlockNumber required')
   }));
 
+  router.post('/recaptureTransactions/byTokenContractAddress/:tokenContractAddress', asyncMiddleware(async (req, res) => { 
+   
+    const fromBlockNumber = parseInt(req.body.fromBlockNumber)
+    const toBlockNumber = parseInt(req.body.toBlockNumber)
+    const tokenContractAddress = req.params.tokenContractAddress
+    if(tokenContractAddress && fromBlockNumber && toBlockNumber){
+      if(fromBlockNumber < toBlockNumber){
+        const filter = {isActive:true, tokenContractAddress}
+        const snapshotMeta = await db.CompetitionTransactionsSnapshotMeta.findOne(filter)
+        if(snapshotMeta){
+          if(toBlockNumber <= parseInt(snapshotMeta.currentBlockNumber)){
+            //fromblock and toblock should be less then  
+          let transations = await bscScanHelper.queryByCABN(snapshotMeta.tokenContractAddress, fromBlockNumber, toBlockNumber);        
+          if(transations.length > 0){
+             const snapShotFilter = {blockNumber:{$gte: fromBlockNumber}, blockNumber:{$lte: toBlockNumber}}
+             await db.CompetitionTransactionsSnapshots.deleteMany(snapShotFilter)
+             await db.CompetitionTransactionsSnapshots.insertMany(transations)
+             return res.http200(`${transations.length} transactions are stored`)
+          }
+          return res.http200('Zero transactions received from BscScan')  
+          }
+               
+        }
+        return res.http200('TokenContractAddress not registered with Cron')
+      }
+      return res.http400('Invalid block Range')
+    }
+    return res.http400('TokenContractAddress, fromBlockNumber and toBlockNumber required')
+    
+
+  }));
 }
