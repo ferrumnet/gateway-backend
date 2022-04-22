ebe6e42df327bda185e0d128db6a35655fbdedcb
Zikriya Mushtaq <zikriamushtaq12@gmail.com>
2022-04-22 15:00:38 +0500

    product model updated

--- app/controllers/api/v1/super-admin/products.js
+++ app/controllers/api/v1/super-admin/products.js
@@ -1,20 +1,20 @@
-const { asyncMiddleware, commonFunctions, utils } = global;
+const {db, asyncMiddleware, commonFunctions, utils } = global;
 
 
 const { isValidObjectId } = require("mongoose");
 module.exports = function (router) {
 
-  router.get("/list", asyncMiddleware(async (req, res) => {   
-    var filter = {}  
+  router.get("/list", asyncMiddleware(async (req, res) => {
+    var filter = {}
     let products = []
 
     if(req.query.isActive){
       filter.isActive = req.query.isActive
-    } 
+    }
     if (req.query.search) {
       let reg = new RegExp(unescape(req.query.search), 'i');
       filter.name = reg
-    }         
+    }
     if(req.query.isPagination != null && req.query.isPagination == 'false'){
       products = await db.Product.find(filter)
       .sort({ createdAt: -1 })
@@ -25,36 +25,42 @@ module.exports = function (router) {
       .limit(req.query.limit ? parseInt(req.query.limit) : 10)
     }
     return res.http200({products})
- 
+
   }));
 
   router.post("/create", asyncMiddleware(async (req, res) => {
-    const payload = {name:req.body.name, createdByUser:req.user.id};
-    if (payload.name) { 
-      const product = await db.Product.create(payload);
-      return res.http200({ product });
+
+    if (!req.body.name) {
+      return res.http400('name is required.');
     }
-      return res.http400("Name is required.");
-    
+
+    req.body.createdByUser = req.user._id
+    req.body.updatedByUser = req.user._id
+    req.body.createdAt = new Date()
+    req.body.updatedAt = new Date()
+
+    const product = await db.Product.create(req.body);
+      return res.http200({ product });
+
   }));
 
   router.put("/update/:id", asyncMiddleware(async (req, res) => {
     const filter = { _id:req.params.id };
-    const payload = {name: req.body.name, createdByUser:req.user._id }   
-   
+    const payload = {name: req.body.name, createdByUser:req.user._id }
+
     if (payload.name && isValidObjectId(filter._id)) {
       const productCount = await db.Product.countDocuments(filter);
       if (productCount > 0) {
         const product =  await db.Product.findOneAndUpdate(filter, payload, { new: true })
         return res.http200({ product });
-      } 
+      }
       return res.http404(
           await commonFunctions.getValueFromStringsPhrase(stringHelper.strErrorProductNotFound),
           stringHelper.strErrorProductNotFound
-        );      
-    } 
+        );
+    }
       return res.http400("Name and valid ID is required.");
-    
+
   }));
 
   router.put("/active/inactive/:id", asyncMiddleware(async (req, res) => {
@@ -63,19 +69,19 @@ module.exports = function (router) {
 
     if (isValidObjectId(filter._id) && typeof payload.isActive == "boolean") {
       const productCount = await db.Product.countDocuments(filter);
-      
+
       if (productCount > 0) {
-        const product = await db.Product.findOneAndUpdate(filter, payload, { new: true });               
+        const product = await db.Product.findOneAndUpdate(filter, payload, { new: true });
         return res.http200({ product });
-      } 
+      }
         return res.http404(
            await commonFunctions.getValueFromStringsPhrase(stringHelper.strErrorProductNotFound ),
           stringHelper.strErrorProductNotFound
         );
-      
+
     }
       return res.http400("Valid id and active is required.");
-    
+
   }));
 
   router.get("/:id", asyncMiddleware(async (req, res) => {
@@ -84,14 +90,14 @@ module.exports = function (router) {
       const product = await  db.Product.findOne(filter);
       if (product) {
         return res.http200({ product });
-      } 
+      }
         return res.http404(
           await commonFunctions.getValueFromStringsPhrase(stringHelper.strErrorProductNotFound ),
           stringHelper.strErrorProductNotFound
         );
-      
-    } 
+
+    }
       return res.http400("Valid id is required.");
-    
+
   }));
 };
