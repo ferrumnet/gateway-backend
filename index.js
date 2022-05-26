46b9587ca299993236e6098fdcdfb4e23a61fd7b
Zikriya Mushtaq <zikriamushtaq12@gmail.com>
2022-05-26 12:33:14 +0500

    crucible get auto calculate apr handled in seprate layer

--- index.js
+++ index.js
@@ -88,6 +88,7 @@ global.organizationHelper = require("./app/lib/middlewares/helpers/organizationH
 global.leaderboardHelper = require("./app/lib/middlewares/helpers/leaderboardHelper");
 global.tokenHolderBalanceSnapshotEventHelper = require("./app/lib/middlewares/helpers/tokenHolderBalanceSnapshotEventHelper");
 global.currencyHelper = require("./app/lib/middlewares/helpers/currencyHelper");
+global.crucibleAprsHelper = require("./app/lib/middlewares/helpers/crucibleAprsHelper");
 
 global.kraken = app.kraken;
 // const whitelist = global.environment.whitelist;
