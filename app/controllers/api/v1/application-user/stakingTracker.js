6365e8b549c5bf64d4ea75c02f4c56e9aeaf22e3
Rizwan Haider <rizwan.haider@ferrum.network>
2022-05-30 16:08:53 +0500

    staking tracker list api moved to public

--- app/controllers/api/v1/application-user/stakingTracker.js
+++ app/controllers/api/v1/application-user/stakingTracker.js
@@ -78,21 +78,4 @@ module.exports = function (router) {
     }
     return res.http404("CABN not found");
   });
-
-  router.get(
-    "/list/stakingContractAddress/:stakingContractAddress/currencyAddressesByNetwork/:currencyAddressesByNetwork",
-    asyncMiddleware(async (req, res) => {
-      const cabn = await db.CurrencyAddressesByNetwork.findOne({
-        _id: req.params.currencyAddressesByNetwork,
-      });
-      if (cabn) {
-        const participants = await stakingTrackerHelper.intiatParticipentsData(
-          cabn._id,
-          req.params.stakingContractAddress
-        );
-        return res.http200({ participants });
-      }
-      return res.http404("CABN not found");
-    })
-  );
 };
