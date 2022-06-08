f24af0eba42695afed05aee29a01a2602ee236a0
Zikriya Mushtaq <zikriamushtaq12@gmail.com>
2022-06-08 20:10:56 +0500

    bug fixing

--- app/lib/crons/fetchCrucibleApr.js
+++ app/lib/crons/fetchCrucibleApr.js
@@ -17,7 +17,7 @@ async function start() {
 
     const scheduler = {};
 
-    let task = cron.schedule('0 */3 * * * *', async () => {
+    let task = cron.schedule('30 */3 * * * *', async () => {
       console.log('fetchCrucibleApr cron triggered:::')
       console.log(new Date())
       triggerJobs()
