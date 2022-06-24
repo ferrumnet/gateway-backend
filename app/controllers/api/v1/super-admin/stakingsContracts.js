7c4cfc1a42c7873c278929cac04d33b0fde26def
Rizwan Haider <rizwan.haider@ferrum.network>
2022-06-24 21:33:32 +0500

    multiStakingleaderboard partical update

--- app/controllers/api/v1/super-admin/stakingsContracts.js
+++ app/controllers/api/v1/super-admin/stakingsContracts.js
@@ -1,16 +1,49 @@
 const { db } = global;
+const { isValidObjectId } = require("mongoose");
 
 module.exports = function (router) {
   router.post("/create", async (req, res) => {
     if (
-      !req.body.stakingContractAddress ||
-      !req.body.currencyAddressesByNetwork
+      (!req.body.stakingContractAddresses && !req.body.stakingContractAddresses.length) ||
+      !req.body.currencyAddressByNetwork
     ) {
       return res.http400(
-        "Staking Contract Address & currencyAddressesByNetwork are required."
+        "Staking Contract Addresses & currencyAddressByNetwork are required."
       );
     }
-    const stakingsContractsAddress = await db.StakingsContractsAddresses.create(req.body);
-    return res.http200({ stakingsContractsAddress,});
+    const stakingsContractsAddress = await db.StakingsContractsAddresses.create(
+      req.body
+    );
+    return res.http200({ stakingsContractsAddress });
+  });
+
+  // update status
+  router.patch("/update/:id", async (req, res) => {
+    const filter = { _id: req.params.id };
+    const payload = { isActive: req.body.isActive };
+
+    if (isValidObjectId(filter._id) && typeof payload.isActive == "boolean") {
+      const stakingContractAddress =
+        await db.StakingsContractsAddresses.findOneAndUpdate(filter, payload, {
+          new: true,
+        });
+      return res.http200({ stakingContractAddress });
+    }
+    return res.http400("Valid id and isActive is required.");
+  });
+
+  // list
+  router.get("/list", async (req, res) => {
+    let stakingContractAddresses;
+    if (req.query.isPagination != null && req.query.isPagination == "false") {
+      stakingContractAddresses = await db.StakingsContractsAddresses.find();
+    } else {
+      stakingContractAddresses = await db.StakingsContractsAddresses.find()
+        .skip(req.query.offset ? parseInt(req.query.offset) : 0)
+        .limit(req.query.limit ? parseInt(req.query.limit) : 10);
+    }
+    return res.http200({
+      stakingContractAddresses,
+    });
   });
 };
--- app/models/stakingsContractsAddresses.js
+++ app/models/stakingsContractsAddresses.js
@@ -5,8 +5,9 @@ var mongoose = require("mongoose");
 var StakingsContractsAddresses = function () {
   var stakingSchema = mongoose.Schema(
     {
-      stakingContractAddress: { type: String, required: true },
-      currencyAddressesByNetwork: [{ type: mongoose.Schema.Types.ObjectId, ref: "currencyAddressesByNetwork", required: true }],
+      leaderboard: { type: mongoose.Schema.Types.ObjectId, ref: 'leaderboards'},
+      stakingContractAddresses: [{ type: String, required: true }],
+      currencyAddressByNetwork: { type: mongoose.Schema.Types.ObjectId, ref: "currencyAddressesByNetwork", required: true },
       isActive: { type: Boolean, default: false },
     },
     {timestamps: true, collection: "stakingsContractsAddresses", timestamps: true }
