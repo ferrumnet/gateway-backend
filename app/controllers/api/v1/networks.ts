84ebda2d2dfaa02c76a5e3c99fb556a3aa16aac2
Zikriya <zikriya.mushtaq@ferrum.network>
2022-12-12 02:43:36 +0500

    Typo error fixed on getAllNetwork api for publicUser

--- app/controllers/api/v1/networks.ts
+++ app/controllers/api/v1/networks.ts
@@ -27,7 +27,7 @@ module.exports = function (router: any) {
   router.get('/list', async (req: any, res: any) => {
 
     var filter: any = {};
-    let netwroks = [];
+    let networks = [];
     var sort: any = { createdAt: -1 }
 
     if (req.query.sortKey) {
@@ -59,12 +59,12 @@ module.exports = function (router: any) {
 
     if (req.query.isPagination != null && req.query.isPagination == 'false') {
 
-      netwroks = await db.Networks.find(filter).populate('parentId')
+      networks = await db.Networks.find(filter).populate('parentId')
         .sort(sort)
 
     } else {
 
-      netwroks = await db.Networks.find(filter).populate('parentId')
+      networks = await db.Networks.find(filter).populate('parentId')
         .sort(sort)
         .skip(req.query.offset ? parseInt(req.query.offset) : 0)
         .limit(req.query.limit ? parseInt(req.query.limit) : 10)
@@ -72,7 +72,7 @@ module.exports = function (router: any) {
     }
 
     return res.http200({
-      netwroks: netwroks
+      networks: networks
     });
 
   });
