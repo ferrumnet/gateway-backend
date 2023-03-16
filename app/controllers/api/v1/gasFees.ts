49adb030b9a66687cddcae5568e087fac74a1c14
Zikriya <zikriya.mushtaq@ferrum.network>
2023-03-16 21:03:42 +0500

    getGasFeesByChainId api created

---
+++ app/controllers/api/v1/gasFees.ts
@@ -0,0 +1,19 @@
+module.exports = function (router: any) {
+
+  router.get('/:chainId', async (req: any, res: any) => {
+
+    var filter: any = {};
+
+    if(req.params.chainId){
+      filter.chainId = req.params.chainId;
+    }
+
+    let gasFees = await db.GasFees.findOne(filter);
+
+    return res.http200({
+      gasFees: gasFees
+    });
+
+  });
+
+};
