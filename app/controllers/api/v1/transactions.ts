b7786887b8695fc78e73cac6d79e91328f2f4fd9
“ArslanKibria” <kibriya.mushtaq@ferrum.network>
2023-03-22 00:30:51 +0500

    getSignedData from node and save onto swapAndWithdraw document

--- app/controllers/api/v1/transactions.ts
+++ app/controllers/api/v1/transactions.ts
@@ -6,13 +6,15 @@ module.exports = function (router: any) {
 
     commonFunctions.doAuthForNodeApis(req);
 
-    if (!req.params.txHash) {
-      return res.http400('txHash is required.');
+    if (!req.params.txHash && req.body.signedData) {
+      return res.http400('txHash & signedData are required.');
     }
+
+    if(req.body.signatures && req.body.signatures.length == 0){
+      return res.http401('signatures can not be empty');
+    }
+
     console.log('update swapAndWitdraw body', req.body);
-    if(req.body.signedData){
-      console.log('update swapAndWitdraw body signedData', req.body.signedData);
-    }
 
     let swapAndWithdrawTransaction = await db.SwapAndWithdrawTransactions.findOne({ receiveTransactionId: req.params.txHash })
       .populate('sourceNetwork').populate('destinationNetwork')
@@ -32,7 +34,7 @@ module.exports = function (router: any) {
       if (transaction) {
         swapAndWithdrawTransaction = await swapTransactionHelper.filterTransactionDetail(swapAndWithdrawTransaction, transaction);
       }
-      
+      swapAndWithdrawTransaction = getSignedData(swapAndWithdrawTransaction, req.body.signedData);
       swapAndWithdrawTransaction.nodeJob.updatedAt = new Date();
       swapAndWithdrawTransaction.updatedAt = new Date();
       swapAndWithdrawTransaction = await db.SwapAndWithdrawTransactions.findOneAndUpdate({ _id: swapAndWithdrawTransaction._id }, swapAndWithdrawTransaction, { new: true });
@@ -42,5 +44,14 @@ module.exports = function (router: any) {
     });
 
   }));
+
+  function getSignedData(swapAndWithdrawTransaction: any,  signedData: any){
+
+    swapAndWithdrawTransaction.payBySig.salt = signedData.salt;
+    swapAndWithdrawTransaction.payBySig.hash = signedData.hash;
+    swapAndWithdrawTransaction.payBySig.signatures = signedData.signatures;
+
+    return swapAndWithdrawTransaction;
+  }
 
 };
