906a7f2370c4a38c5d851d83b57f18a084ece501
“ArslanKibria” <kibriya.mushtaq@ferrum.network>
2023-05-11 22:48:40 +0500

    updateSwapAndWithdrawJobByJobId api formatted and also some modification done to support non-evm

--- app/controllers/api/v1/transactions.ts
+++ app/controllers/api/v1/transactions.ts
@@ -1,70 +1,99 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 
 module.exports = function (router: any) {
+  router.put(
+    "/update/swap/and/withdraw/job/:txHash",
+    asyncMiddleware(async (req: any, res: any) => {
+      commonFunctions.doAuthForNodeApis(req);
 
-  router.put('/update/swap/and/withdraw/job/:txHash', asyncMiddleware(async (req: any, res: any) => {
-
-    commonFunctions.doAuthForNodeApis(req);
-
-    if (req.query.isFrom == 'fiber') {
-      await handleFIBERRequest(req);
-    } else {
-      if (!req.params.txHash && req.body.signedData) {
-        return res.http400('txHash & signedData are required.');
-      }
-
-      if (req.body.signatures && req.body.signatures.length == 0) {
-        return res.http401('signatures can not be empty');
-      }
-
-      console.log('update swapAndWitdraw body', req.body);
-
-      let swapAndWithdrawTransaction = await db.SwapAndWithdrawTransactions.findOne({ receiveTransactionId: req.params.txHash, status: utils.swapAndWithdrawTransactionStatuses.swapPending })
-        .populate('sourceNetwork').populate('destinationNetwork')
-        .populate('sourceCabn').populate('destinationCabn');
-
-      if (req.body && swapAndWithdrawTransaction) {
-        let transaction = req.body.transaction;
-        let transactionReceipt = req?.body?.transactionReceipt;
-        swapAndWithdrawTransaction.nodeJob.status = utils.swapAndWithdrawTransactionJobStatuses.completed;
-
-        if (transactionReceipt?.status && transactionReceipt?.status == true) {
-          swapAndWithdrawTransaction.status = utils.swapAndWithdrawTransactionStatuses.swapCompleted;
-        } else {
-          swapAndWithdrawTransaction.status = utils.swapAndWithdrawTransactionStatuses.swapFailed;
+      if (req.query.isFrom == "fiber") {
+        await handleFIBERRequest(req);
+      } else {
+        if (!req.params.txHash && req.body.signedData) {
+          return res.http400("txHash & signedData are required.");
         }
 
-        swapAndWithdrawTransaction = await getTransactionDetail(swapAndWithdrawTransaction, req.body.signedData);
-        swapAndWithdrawTransaction = getSignedData(swapAndWithdrawTransaction, req.body.signedData);
-        swapAndWithdrawTransaction.nodeJob.updatedAt = new Date();
-        swapAndWithdrawTransaction.updatedAt = new Date();
-        swapAndWithdrawTransaction = await db.SwapAndWithdrawTransactions.findOneAndUpdate({ _id: swapAndWithdrawTransaction._id }, swapAndWithdrawTransaction, { new: true });
+        if (req.body.signatures && req.body.signatures.length == 0) {
+          return res.http401("signatures can not be empty");
+        }
+
+        console.log("update swapAndWitdraw body", req.body);
+
+        let swapAndWithdrawTransaction =
+          await db.SwapAndWithdrawTransactions.findOne({
+            receiveTransactionId: req.params.txHash,
+            status: utils.swapAndWithdrawTransactionStatuses.swapPending,
+          })
+            .populate("sourceNetwork")
+            .populate("destinationNetwork")
+            .populate("sourceCabn")
+            .populate("destinationCabn");
+
+        if (req.body && swapAndWithdrawTransaction) {
+          let transaction = req.body.transaction;
+          let transactionReceipt = req?.body?.transactionReceipt;
+          swapAndWithdrawTransaction.nodeJob.status =
+            utils.swapAndWithdrawTransactionJobStatuses.completed;
+
+          if (
+            transactionReceipt?.status &&
+            transactionReceipt?.status == true
+          ) {
+            swapAndWithdrawTransaction.status =
+              utils.swapAndWithdrawTransactionStatuses.swapCompleted;
+          } else {
+            swapAndWithdrawTransaction.status =
+              utils.swapAndWithdrawTransactionStatuses.swapFailed;
+          }
+
+          swapAndWithdrawTransaction = await getTransactionDetail(
+            swapAndWithdrawTransaction,
+            req.body.signedData
+          );
+          swapAndWithdrawTransaction = getSignedData(
+            swapAndWithdrawTransaction,
+            req.body.signedData
+          );
+          swapAndWithdrawTransaction.nodeJob.updatedAt = new Date();
+          swapAndWithdrawTransaction.updatedAt = new Date();
+          swapAndWithdrawTransaction =
+            await db.SwapAndWithdrawTransactions.findOneAndUpdate(
+              { _id: swapAndWithdrawTransaction._id },
+              swapAndWithdrawTransaction,
+              { new: true }
+            );
+        }
       }
-    }
-    return res.http200({
-      message: stringHelper.strSuccess
-    });
+      return res.http200({
+        message: stringHelper.strSuccess,
+      });
+    })
+  );
 
-  }));
-
-  async function getTransactionDetail(swapAndWithdrawTransaction: any, signedData: any) {
-
+  async function getTransactionDetail(
+    swapAndWithdrawTransaction: any,
+    signedData: any
+  ) {
     try {
       swapAndWithdrawTransaction.sourceAmount = signedData.amount;
       swapAndWithdrawTransaction.sourceWalletAddress = signedData.from;
-      swapAndWithdrawTransaction.destinationWalletAddress = signedData.targetAddress;
+      swapAndWithdrawTransaction.destinationWalletAddress =
+        signedData.targetAddress;
       if (swapAndWithdrawTransaction.sourceAmount) {
-        swapAndWithdrawTransaction.sourceAmount = await swapUtilsHelper.amountToHuman_(swapAndWithdrawTransaction.sourceNetwork, swapAndWithdrawTransaction.sourceCabn, swapAndWithdrawTransaction.sourceAmount);
+        swapAndWithdrawTransaction.sourceAmount =
+          await swapUtilsHelper.amountToHuman_(
+            swapAndWithdrawTransaction.sourceNetwork,
+            swapAndWithdrawTransaction.sourceCabn,
+            swapAndWithdrawTransaction.sourceAmount
+          );
       }
     } catch (e) {
       console.log(e);
     }
     return swapAndWithdrawTransaction;
-
   }
 
   function getSignedData(swapAndWithdrawTransaction: any, signedData: any) {
-
     try {
       swapAndWithdrawTransaction.payBySig.salt = signedData.salt;
       swapAndWithdrawTransaction.payBySig.hash = signedData.hash;
@@ -73,7 +102,6 @@ module.exports = function (router: any) {
       console.log(e);
     }
     return swapAndWithdrawTransaction;
-
   }
 
   async function handleFIBERRequest(req: any) {
@@ -81,44 +109,61 @@ module.exports = function (router: any) {
       if (req.body && req.params.txHash) {
         let withdrawData = req.body;
         if (withdrawData) {
-          console.log('doSwapAndWithdraw withdrawHash', withdrawData);
-          let swapAndWithdrawTransactionObject = await db.SwapAndWithdrawTransactions.findOne({ receiveTransactionId: req.params.txHash });
+          console.log("doSwapAndWithdraw withdrawHash", withdrawData);
+          let swapAndWithdrawTransactionObject =
+            await db.SwapAndWithdrawTransactions.findOne({
+              receiveTransactionId: req.params.txHash,
+            });
 
           let filter: any = {};
           filter._id = swapAndWithdrawTransactionObject._id;
 
           if (!swapAndWithdrawTransactionObject) {
-            throw 'Invalid operation';
+            throw "Invalid operation";
           }
 
           let useTransaction = {
             transactionId: withdrawData.data,
-            status: utils.swapAndWithdrawTransactionStatuses.swapWithdrawCompleted,
-            timestamp: new Date()
-          }
+            status:
+              utils.swapAndWithdrawTransactionStatuses.swapWithdrawCompleted,
+            timestamp: new Date(),
+          };
 
-          if (swapAndWithdrawTransactionObject.useTransactions && swapAndWithdrawTransactionObject.useTransactions.length > 0) {
-            let txItem = (swapAndWithdrawTransactionObject.useTransactions || []).find((t: any) => t.transactionId === withdrawData.data);
+          if (
+            swapAndWithdrawTransactionObject.useTransactions &&
+            swapAndWithdrawTransactionObject.useTransactions.length > 0
+          ) {
+            let txItem = (
+              swapAndWithdrawTransactionObject.useTransactions || []
+            ).find((t: any) => t.transactionId === withdrawData.data);
             if (!txItem) {
-              swapAndWithdrawTransactionObject.useTransactions.push(useTransaction);
+              swapAndWithdrawTransactionObject.useTransactions.push(
+                useTransaction
+              );
             }
           } else {
-            swapAndWithdrawTransactionObject.useTransactions.push(useTransaction);
+            swapAndWithdrawTransactionObject.useTransactions.push(
+              useTransaction
+            );
           }
 
           if (withdrawData.withdraw.destinationAmount) {
-            swapAndWithdrawTransactionObject.destinationAmount = withdrawData.withdraw.destinationAmount;
+            swapAndWithdrawTransactionObject.destinationAmount =
+              withdrawData.withdraw.destinationAmount;
           }
 
-          swapAndWithdrawTransactionObject.status = utils.swapAndWithdrawTransactionStatuses.swapWithdrawCompleted;
+          swapAndWithdrawTransactionObject.status =
+            utils.swapAndWithdrawTransactionStatuses.swapWithdrawCompleted;
           swapAndWithdrawTransactionObject.updatedAt = new Date();
-          swapAndWithdrawTransactionObject = await db.SwapAndWithdrawTransactions.findOneAndUpdate(filter, swapAndWithdrawTransactionObject);
+          swapAndWithdrawTransactionObject =
+            await db.SwapAndWithdrawTransactions.findOneAndUpdate(
+              filter,
+              swapAndWithdrawTransactionObject
+            );
         }
-
       }
     } catch (e) {
       console.log(e);
     }
   }
-
 };
