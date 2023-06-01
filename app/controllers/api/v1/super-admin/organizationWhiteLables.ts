77b77a8ca74d27e1034bdada3cc5b220cf452da2
“ArslanKibria” <kibriya.mushtaq@ferrum.network>
2023-06-01 15:48:20 +0500

    createOrganizationWhiteLables api for superAdmin modified

--- app/controllers/api/v1/super-admin/organizationWhiteLables.ts
+++ app/controllers/api/v1/super-admin/organizationWhiteLables.ts
@@ -1,81 +1,23 @@
-
 module.exports = function (router: any) {
-
-  router.post('/create', async (req: any, res: any) => {
-
-    if (!req.body.headerHtml || !req.body.footerHtml || !req.body.organization) {
-      return res.http400('headerHtml & footerHtml & organization are required.');
+  router.post("/create", async (req: any, res: any) => {
+    if (!req.body.backgroundImage || !req.body.tittle || !req.body.type) {
+      return res.http400("backgroundImage & tittle & type are required.");
     }
 
-    req.body.createdByUser = req.user._id
-    req.body.updatedByUser = req.user._id
-    req.body.createdByOrganization = req.body.organization
-    req.body.createdAt = new Date()
-    req.body.updatedAt = new Date()
-
-    let organizationWhiteLables = await db.OrganizationWhiteLables.create(req.body)
-
-    return res.http200({
-      organizationWhiteLables: organizationWhiteLables
-    });
-
-  });
-
-  router.put('/update/:id', async (req: any, res: any) => {
-
-    let filter = {}
-    filter = { _id: req.params.id }
-
-    if (!req.body.headerHtml || !req.body.footerHtml) {
-      return res.http400('headerHtml & footerHtml are required.');
+    req.body.createdByUser = req.user._id;
+    req.body.updatedByUser = req.user._id;
+    if (req.body.organization) {
+      req.body.createdByOrganization = req.body.organizationId;
     }
+    req.body.createdAt = new Date();
+    req.body.updatedAt = new Date();
 
-    req.body.updatedByUser = req.user._id
-    req.body.updatedAt = new Date()
-
-    let organizationWhiteLables = await db.OrganizationWhiteLables.findOneAndUpdate(filter, req.body, { new: true })
+    let organizationWhiteLables = await db.OrganizationWhiteLables.create(
+      req.body
+    );
 
     return res.http200({
-      organizationWhiteLables: organizationWhiteLables
+      organizationWhiteLables: organizationWhiteLables,
     });
-
   });
-
-  router.get('/list', async (req: any, res: any) => {
-
-    var filter = {}
-
-    let organizationWhiteLables = await db.OrganizationWhiteLables.find(filter)
-      .sort({ createdAt: -1 })
-      .skip(req.query.offset ? parseInt(req.query.offset) : 0)
-      .limit(req.query.limit ? parseInt(req.query.limit) : 10)
-
-    return res.http200({
-      organizationWhiteLables: organizationWhiteLables
-    });
-
-  });
-
-  router.get('/:id', async (req: any, res: any) => {
-    let filter = {}
-    filter = { _id: req.params.id }
-
-    let organizationWhiteLables = await db.OrganizationWhiteLables.findOne(filter)
-
-    return res.http200({
-      organizationWhiteLables: organizationWhiteLables
-    });
-
-  });
-
-  router.delete('/:id', async (req: any, res: any) => {
-    let filter = {}
-
-    let organizationWhiteLables = await db.OrganizationWhiteLables.remove({ _id: req.params.id })
-
-    return res.http200({
-      organizationWhiteLables: organizationWhiteLables
-    });
-  })
-
 };
