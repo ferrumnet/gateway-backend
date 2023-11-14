4371ddec843455ec1a4e58bc4d9d473fe88a3ead
Zikriya <zikriya.mushtaq@ferrum.network>
2023-11-14 18:29:32 +0500

    leaderboard and organizationWhiteLables updated

--- app/models/leaderboards.ts
+++ app/models/leaderboards.ts
@@ -1,39 +1,86 @@
-'use strict';
-var mongoose = require('mongoose');
+"use strict";
+var mongoose = require("mongoose");
 
-var schema = mongoose.Schema({
-  name: { type: String, default: "" },
-  nameInLower: { type: String, default: "" },
-  exclusionWalletAddressList: [],
-  leaderboardCurrencyAddressesByNetwork: [{ type: mongoose.Schema.Types.ObjectId, ref: 'leaderboardCurrencyAddressesByNetwork' }],
-  status: { type: String, default: "pending" },
-  isPublished: { type: Boolean, default: false },
-  isActive: { type: Boolean, default: true },
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "users", required: true },
-  createdByUser: { type: mongoose.Schema.Types.ObjectId, ref: "users", required: true }, // this key needs be replaced on all over the app. Right now key user is used on every where. So once this will replaced we will remove user from the model
-  updatedByUser: { type: mongoose.Schema.Types.ObjectId, ref: "users" },
-  organization: { type: mongoose.Schema.Types.ObjectId, ref: 'organizations', required: true },
-  isVisibleForPublicMenuItem: { type: Boolean, default: true },
-  leaderboardStakingContractAddresses: [{ type: mongoose.Schema.Types.ObjectId, ref: 'stakingsContractsAddresses'}],
-  stakingContractAddresses: [],
-  type: { type: String, enum: { values: ['stake', 'other'], message: 'Invalid type' }, default: 'other' },
-  customCurrencyAddressesByNetwork: [
-    {
-      postion: { type: Number, default: 0 },
-      from: {
-        symble: { type: String, default: "" },
-        currencyAddressesByNetwork: { type: mongoose.Schema.Types.ObjectId, ref: 'currencyAddressesByNetwork' },
+var schema = mongoose.Schema(
+  {
+    name: { type: String, default: "" },
+    nameInLower: { type: String, default: "" },
+    exclusionWalletAddressList: [],
+    leaderboardCurrencyAddressesByNetwork: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "leaderboardCurrencyAddressesByNetwork",
       },
-      to: {
-        symble: { type: String, default: "" },
-        currencyAddressesByNetwork: { type: mongoose.Schema.Types.ObjectId, ref: 'currencyAddressesByNetwork' },
-      }
-    }
-  ],
+    ],
+    status: { type: String, default: "pending" },
+    isPublished: { type: Boolean, default: false },
+    isActive: { type: Boolean, default: true },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "users",
+      required: true,
+    },
+    createdByUser: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "users",
+      required: true,
+    }, // this key needs be replaced on all over the app. Right now key user is used on every where. So once this will replaced we will remove user from the model
+    updatedByUser: { type: mongoose.Schema.Types.ObjectId, ref: "users" },
+    organization: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "organizations",
+      required: true,
+    },
+    isVisibleForPublicMenuItem: { type: Boolean, default: true },
+    leaderboardStakingContractAddresses: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "stakingsContractsAddresses",
+      },
+    ],
+    stakingContractAddresses: [],
+    type: {
+      type: String,
+      enum: { values: ["stake", "other"], message: "Invalid type" },
+      default: "other",
+    },
+    customCurrencyAddressesByNetwork: [
+      {
+        postion: { type: Number, default: 0 },
+        from: {
+          symble: { type: String, default: "" },
+          currencyAddressesByNetwork: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "currencyAddressesByNetwork",
+          },
+        },
+        to: {
+          symble: { type: String, default: "" },
+          currencyAddressesByNetwork: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "currencyAddressesByNetwork",
+          },
+        },
+      },
+    ],
+    snapHodlConfigId: { type: String, default: "" },
+    snapShotConfigName: { type: String, default: "" },
+    stakingContractDataBalances: [
+      {
+        stakingContractAddress: { type: String, default: "" },
+        tokenContractAddress: { type: String, default: "" },
+        chainId: { type: String, default: "" },
+        totalStakedBalance: { type: String, default: "" },
+      },
+    ],
+    levelUpUrl: { type: String, default: "" },
+    balanceColumnName: { type: String, default: "" },
 
-  createdAt: { type: Date, default: new Date() },
-  updatedAt: { type: Date, default: new Date() },
-}, { collection: 'leaderboards' });
+    createdAt: { type: Date, default: new Date() },
+    updatedAt: { type: Date, default: new Date() },
+  },
+  { collection: "leaderboards" }
+);
 
-var leaderboardsModel = mongoose.model("leaderboards",schema);
+var leaderboardsModel = mongoose.model("leaderboards", schema);
 module.exports = leaderboardsModel;
