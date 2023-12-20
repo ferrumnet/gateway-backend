5e12186be87a3718869fdc3600b89a77b22ce1e0
Zikriya <zikriya.mushtaq@ferrum.network>
2023-12-20 21:19:48 +0500

    transaction.ts updated

--- app/controllers/api/v1/community-member/multiSwap/transactions.ts
+++ app/controllers/api/v1/community-member/multiSwap/transactions.ts
@@ -7,6 +7,28 @@ import {
 import { postMultiswapAlertIntoChannel } from "../../../../../lib/httpCalls/slackAxiosHelper";
 
 module.exports = function (router: any) {
+  router.post("/do/swap/and/withdraw/:swapTxId", async (req: any, res: any) => {
+    swapTransactionHelper.validationForDoSwapAndWithdraw(req);
+    req.sourceNetwork = await db.Networks.findOne({
+      _id: req.query.sourceNetworkId,
+    });
+    req.destinationNetwork = await db.Networks.findOne({
+      _id: req.query.destinationNetworkId,
+    });
+    if (!req.sourceNetwork || !req.destinationNetwork) {
+      throw "Invalid sourceNetwork or destinationNetwork";
+    }
+    let swapAndWithdrawTransaction =
+      await swapTransactionHelper.createPendingSwap(req);
+    swapAndWithdrawTransaction = await swapTransactionHelper.doSwapAndWithdraw(
+      req,
+      swapAndWithdrawTransaction
+    );
+    return res.http200({
+      swapAndWithdrawTransaction: swapAndWithdrawTransaction,
+    });
+  });
+
   router.post(
     "/regenerate/swap/and/withdraw/:txId",
     asyncMiddleware(async (req: any, res: any) => {
