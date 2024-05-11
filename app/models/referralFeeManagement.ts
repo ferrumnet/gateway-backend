23e1b8487c99a2527d793b7f3681f89182cb8014
Abdul Ahad <9919+AbdulAhadArain@users.noreply.github.com>
2024-05-11 19:34:04 +0500

    fee distribution api and schema updated

--- app/models/referralFeeManagement.ts
+++ app/models/referralFeeManagement.ts
@@ -5,7 +5,7 @@ var collectionName = "referralFeeManagement";
 
 var schema = mongoose.Schema(
   {
-    tier: { type: String, default: "" },
+    tier: { type: String, default: "General", unique: true },
     fee: { type: Number, default: 0 },
     feeType: { type: String, enum: { values: ["PERCENTAGE", "ABSOLUTE"] } },
     userAddresses: { type: [String], default: [] },
