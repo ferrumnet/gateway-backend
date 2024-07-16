b1ae25db6281b624d4a1a07735581ba45132608b
Zikriya <zikriya.mushtaq@ferrum.network>
2024-07-17 00:55:48 +0500

    SuperTest, jest setup. Few apis like getAllCabns, getAllNetworks, getAllWabns test cases

--- app/models/index.ts
+++ app/models/index.ts
@@ -1,7 +1,8 @@
-var fs = require('fs')
+var fs = require("fs");
 fs.readdirSync(__dirname).forEach(function (file: any) {
-  if (file !== 'plugins' && file !== 'index.js' ) {
-    let moduleName = file.split('.')[0]
-    exports[moduleName[0].toUpperCase() + moduleName.slice(1)] = require('./' + moduleName)
+  if (file !== "plugins" && file !== "index.js" && file !== "README.md") {
+    let moduleName = file.split(".")[0];
+    exports[moduleName[0].toUpperCase() + moduleName.slice(1)] = require("./" +
+      moduleName);
   }
-})
+});
---
+++ tests/v1/getAllWbns.test.ts
@@ -0,0 +1,23 @@
+import { baseURL } from "../constants/constants";
+const validApiUrl = "/api/v1/wallets/wbn/list";
+const inValidApiUrl = "/api/v1/wallets/wbn/nonexistent";
+var request = require("supertest");
+
+describe(`GET ${validApiUrl}`, () => {
+  it("should return a list of wallets with status 200", async () => {
+    const res = await request(baseURL).get(validApiUrl);
+    console.log(res.body.body);
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.body).toHaveProperty("walletByNetworks");
+  });
+
+  it("should handle non-existent endpoint with status 404", async () => {
+    const res = await request(baseURL).get(inValidApiUrl);
+    expect(res.statusCode).toEqual(404);
+  });
+
+  it("should return 404 for invalid method (POST)", async () => {
+    const res = await request(baseURL).post(validApiUrl);
+    expect(res.statusCode).toEqual(404);
+  });
+});
