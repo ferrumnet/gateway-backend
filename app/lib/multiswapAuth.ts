d16638ffd09a7314304c7f325bee706a7b49dfd1
Zikriya <zikriya.mushtaq@ferrum.network>
2024-09-02 18:12:20 +0500

    multiswapAuth added

---
+++ app/lib/multiswapAuth.ts
@@ -0,0 +1,31 @@
+import {
+  decodeToken,
+  getUser,
+  invalidRequest,
+  Response,
+} from "../helpers/authHelpers/rootAuthHelper";
+const AUTHORIZATION_MISSING = "Authorization header missing";
+
+module.exports = function () {
+  return async function (req: any, res: any, next: any) {
+    if (!req.headers.authorization) {
+      return res.http401(AUTHORIZATION_MISSING);
+    }
+
+    try {
+      let response: Response = decodeToken(req);
+      if (!response?.isValid) {
+        return invalidRequest(res);
+      }
+      const user = await getUser(response.id, response?.role);
+      if (!user) {
+        return invalidRequest(res);
+      }
+      req.user = user;
+      next();
+    } catch (error) {
+      console.log(error);
+      return invalidRequest(res);
+    }
+  };
+};
